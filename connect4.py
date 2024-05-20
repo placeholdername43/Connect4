44a6a8600b4300862768738912c0c50b16733d2f
Finnadip <42492360+Finnadip@users.noreply.github.com>
2024-05-20 19:44:29 +0100

    Update connect4.py

--- connect4.py
+++ connect4.py
@@ -4,26 +4,7 @@ import numpy as np
 from dataclasses import dataclass
 
 # connect4 - a simple connect 4 game made using functional programming
-"""
-plan
 
-1. allow a player to chose multiplayer or singleplayer thru the commandline
-2. if multiplayer, allow 2 players to play
-3. if singleplayer, allow the player to play against the computer
-    3.1 the computer should use a minimax algorithm to determine the best move
-    3.2 the singleplayer function should call another function with the acc game logic
-4. allow the player to chose the difficulty of the computer and/or auto tune it
-5. represent the two players using x/o
-6. reprsent the board using a matrix
-7. allow the player to chose a column to place their piece
-8. remember to consider the gravity of the pieces, they must fall to the bottom
-9. when a piece is placed, check if the player has won
-10. if the player has won, end the game and display the winner
-11. if the player has not won, switch to the next player
-12. if the board is full and no player has won, end the game and display a draw
-13. allow the player to play again or quit
-
-"""
 MAIN_MENU_OPTION = "Enter 1 for Single Player, 2 for Multi Player, or 3 to Exit: "
 TOKEN_MENU_OPTION = "Enter R to play as the red token or Y to play as the yellow token: "
 TOKEN_PLACEMENT_COLUMN = "What column do you want the token in?, between (1-7): "
@@ -42,6 +23,22 @@ class MainMenuOption(Enum):
             case "2" | "multiplayer": return MainMenuOption.MultiPlayer
             case "3" | "exit": return MainMenuOption.Exit
             case _: return None
+
+class BoundedStack: 
+    def __init__(self, capacity):
+        self.items = []
+        self._maxSize = capacity
+        assert(capacity >= 0), "not positive"
+
+        try:
+            capacity = int(capacity)
+        except TypeError as inst:
+            print("Error", inst.args)
+        except:
+            print("error")
+        else:
+            itemmax = capacity
+
 
 
                         
@@ -64,35 +61,46 @@ Cell: TypeAlias = Optional[Token] # define the cell type
 # Another datastructure could be a stack 
 Column: TypeAlias = Tuple[Cell, Cell, Cell, Cell, Cell, Cell] # defining a column as a tuple of 6 cells reflecting the 6 high grid of connect 4
 Grid: TypeAlias = Tuple[Column, Column, Column, Column, Column, Column, Column] # a grid is therefore a tuple of 7 columns reflecting the 7 high grid of connect4
-grid: Grid = list(list(None for _ in range(6)) for _ in range(7)) # the grid is therefore created as a first having empty values in each row, as no tokens are placed initially
+
+def create_empty_grid() -> Grid:
+    return tuple(tuple(None for _ in range(6)) for _ in range(7))
+
+
+"""
+# Bounded Stack
+
+
+
+"""
+
 
 
 
 def print_grid(grid: Grid):
     for row in range(5, -1, -1):
         for column in grid:
-            cell = column[row] # a cell is a point intersecting a certain row and column
+            cell = column[row]
             print(cell if cell is not None else '[ ]', end=' ')
-        print(row)
-    for i in range(1, 8, 1):
-        print(f'{i:^3}', end = ' ')
+        print()
+    for i in range(1, 8):
+        print(f'{i:^3}', end=' ')
+    print()
+
     
 def prompt_for_token_placement() -> int:
     while True:
         n: int = int(input(TOKEN_PLACEMENT_COLUMN))
-        if 1 <= n <= 6:
-            return n
+        if 1 <= n <= 7:
+            return n - 1
         else:
             print("Invalid input, please enter a number between 1-7")
 
-def provide_column_specified_by_user_to_place_in(n):
-    return grid[n-1]
+
 
 def check_any_empty_cell(c: Column):
     for i in range(0,len (c) - 1):
         if c[i] == None:
             return i
-            
         return None
 
 def drop_token_in_column(c:Column, t:Token) -> Optional[Column]:
@@ -102,15 +110,74 @@ def drop_token_in_column(c:Column, t:Token) -> Optional[Column]:
         case i:
             temp = list(c)
             temp[i] = t
-            c = tuple(temp)
-            c = Column
-            return c
+            return tuple(temp)
         
-def place_new_column_in_grid(columnIdx, newColumn: Column) -> Grid:
-    print(grid[columnIdx])
-    grid[columnIdx] 
+def place_new_column_in_grid(grid: Grid, column_idx: int, new_column: Column) -> Grid:
+    return grid[:column_idx] + (new_column,) + grid[column_idx + 1:]
+        
+
+
+
+def prompt_for_token_type() -> Optional[Token]:
+    while True:
+        s: str = input(TOKEN_MENU_OPTION)
+        match s.lower():
+            case "r" | "red" | "1":
+                return R()
+            case "y" | "yellow" | "2":
+                return Y()
+            case _:
+                return None
+
+
+
+def singleplayer(grid: Grid):
+    ChosenToken = prompt_for_token_type()
+    print_grid(grid)
+    chosenColIdx = prompt_for_token_placement()
+    chosenCol = grid[chosenColIdx]
+    newCol = drop_token_in_column(chosenCol, ChosenToken)
+    if newCol:
+        grid = place_new_column_in_grid(grid, chosenColIdx, newCol)
+    else:
+        print("Column is full!")
+    print_grid(grid)
     return grid
+    
+
+    
+
+def multiplayer():
+    pass
+
+def exit():
+    quit()
+
+def prompt_for_main_menu_input():
+    while True:
+        match MainMenuOption.parse(input(MAIN_MENU_OPTION)):
+            case x if type(x) == MainMenuOption:
+                return x
+            case _:
+                print("ERROR: not a valid menu option")
+
+def branch_to_game_feature(opt: MainMenuOption):
+    """prompt for menu option
+    then branch to a feature"""
+    match opt:
+        case MainMenuOption.SinglePlayer:
+            singleplayer(Grid)
+        case MainMenuOption.MultiPlayer:
+            print("Multi Player")
+        case MainMenuOption.Exit:
+            print("Goodbye")
+            return
         
+    
+if __name__ == "__main__":
+    branch_to_game_feature(prompt_for_main_menu_input())
+
+
 
 """
     for row in range(5, -1, -1):
@@ -146,56 +213,3 @@ def prompt_for_token():
         match parse_Token(input(TOKEN_MENU_OPTION)):
             case _ as Token:
                 return Token"""
-
-def prompt_for_token_type() -> Optional[Token]:
-    while True:
-        s: str = input(TOKEN_MENU_OPTION)
-        match s.lower():
-            case "r" | "red" | "1":
-                return R()
-            case "y" | "yellow" | "2":
-                return Y()
-            case _:
-                return None
-
-def singleplayer():
-    prompt_for_main_menu_input()
-    ChosenToken = prompt_for_token_type()
-    chosenColIdx = prompt_for_token_placement()
-    chosenCol = provide_column_specified_by_user_to_place_in(chosenColIdx)
-    newCol = drop_token_in_column(chosenCol, ChosenToken)
-    place_new_column_in_grid(chosenColIdx, newCol)
-    print_grid(grid)
-    
-
-    
-
-def multiplayer():
-    pass
-
-def exit():
-    quit()
-
-def prompt_for_main_menu_input():
-    while True:
-        match MainMenuOption.parse(input(MAIN_MENU_OPTION)):
-            case x if type(x) == MainMenuOption:
-                return x
-            case _:
-                print("ERROR: not a valid menu option")
-
-def branch_to_game_feature(opt: MainMenuOption):
-    """prompt for menu option
-    then branch to a feature"""
-    match opt:
-        case MainMenuOption.SinglePlayer:
-            singleplayer()
-        case MainMenuOption.MultiPlayer:
-            print("Multi Player")
-        case MainMenuOption.Exit:
-            print("Goodbye")
-            return
-        
-    
-if __name__ == "__main__":
-    branch_to_game_feature(prompt_for_main_menu_input())
